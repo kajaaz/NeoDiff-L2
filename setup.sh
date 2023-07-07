2a50b5f9cf6ee2efd2455cee4920f8f6beb1fc0a
root <karolinagorna@hotmail.fr>
2023-07-07 13:05:42 +0000

    update the config file with offchainlabs-goethereum

--- setup.sh
+++ setup.sh
@@ -5,18 +5,15 @@ git submodule init
 git submodule update
 
 echo "building go-ethereum (install go for this)"
-make -C ./go-ethereum all
+make -C ./go-ethereum-clone all
 
 echo "building offchainlabs-go-ethereum (install go for this)"
 make -C ./offchainlabs-go-ethereum all
-cd ../../..
 
-echo "Installing reqs for us, creating virtualenv"
+# echo "Installing reqs for us, creating virtualenv"
 virtualenv -p "$(command -v python3)" "$(pwd)/.env"
 . .env/bin/activate
 pip install -r requirements.txt
-
-cd ..
 
 echo "Done building :)"
 echo "Activate the virtualenv with:"
--- utils/EVMrun.sh
+++ utils/EVMrun.sh
@@ -2,10 +2,17 @@
 
 # start EVMFuzz.py
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd "SCRIPT_DIR"/..
+#SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+#cd "SCRIPT_DIR"/..
+
+#set -x
+# parameter is the seed
+#while :; do ./EVMFuzz.py --name A_20p2000 --seed "$1" --roundsize 2000 --probability 20; echo "Crashed! Restarting"; sleep 1 ; done
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+cd "$SCRIPT_DIR"/..
 
 set -x
 # parameter is the seed
-while :; do ./EVMFuzz.py --name A_20p2000 --seed "$1" --roundsize 2000 --probability 20; echo "Crashed! Restarting"; sleep 1 ; done
+while :; do ./EVMFuzz.py --name A_20p2000 --seed "$1" --roundsize 2000 --probability 20; echo "Crashed! Restarting"; sleep 1; done
 
